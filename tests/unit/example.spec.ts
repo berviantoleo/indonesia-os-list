75e4ca4368d1d5b35afed47b559a6f697166e771
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2021-05-23 16:21:52 +0700

    Add missing files

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,12 +1,9 @@
 import { shallowMount } from "@vue/test-utils";
-import HelloWorld from "@/components/HelloWorld.vue";
+import About from "@/views/About.vue";
 
-describe("HelloWorld.vue", () => {
-  it("renders props.msg when passed", () => {
-    const msg = "new message";
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
-    });
-    expect(wrapper.text()).toMatch(msg);
+describe("About.vue", () => {
+  it("Renders correctly", () => {
+    const wrapper = shallowMount(About);
+    expect(wrapper.text()).toMatch("Indonesia Operating System List");
   });
 });
